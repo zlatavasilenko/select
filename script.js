9bcfd4ff3d265686c1efeb2af97d9ed44a289ae3
fair352 <tatoshka1313@gmail.com>
2022-06-27 19:41:51 +0300

    work2

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const options = [
-    {value: 1, label: 'Витебск'},
-    {value: 2, label: 'Могилёв'},
-    {value: 3, label: 'Гомель'},
-    {value: 4, label: 'Брест'},
-    {value: 5, label: 'Минск'},
+    {value: 1, label: 'Витебск', group: 'Первая'},
+    {value: 2, label: 'Могилёв', group: 'вторая'},
+    {value: 3, label: 'Гомель', group: 'вторая'},
+    {value: 4, label: 'Брест', group: 'Четвёртая'},
+    {value: 5, label: 'Минск', group: 'Четвёртая'},
 ]
-
 class DropDown {
     constructor(selector, options) {
         this.$select = document.querySelector(selector);
@@ -25,32 +24,43 @@ class DropDown {
                     this.selectedItem(e.target.dataset.id)
                 }
             }
-
-        })
-
+        })/*
         this.itemsHTML = this.options.map(({value, label}) => {
             return `<li data-id=${value}>${label}</li>`
-        }).join('')
+        }).join('')*/
+        this.itemsHTML = this.initGroup(this.options).map(([key, items]) => {
+            if (key) {
+                const groupList = items.map(({value, label}) => `<li data-id=${value}>${label}</li>`).join()
+                return `<ul><span>${key}</span>${groupList}</ul>`
+            } else {
+                return items.map(({value, label}) => `<li data-id=${value}>${label}</li>`).join('')
+            }
 
+        }).join('')
 
         this.$dropDown.insertAdjacentHTML('afterbegin', this.itemsHTML)
     }
-
     selectedItem(id) {
         this.$label.innerHTML = this.options.find(item => item.value === id).label
         this.close()
     }
-
-
     open() {
         this.$select.classList.add('active')
     }
-
     close() {
         this.$select.classList.remove('active')
     }
-
+    initGroup(items) {
+        const group = new Map()
+        items.forEach(item => {
+            if (!group.has(item.group)) {
+                group.set(item.group, [item])
+            } else {
+                group.set(item.group, [...group.get(item.group), item])
+            }
+        })
+        return Array.from(group.entries())
+    }
 }
-
 const customSelect = new DropDown('.select', options)
 console.log(customSelect)
